050a0ac0a088b8204056f80f525c5906f909498d
Trevor Redfern <trevorredfern@gmail.com>
2020-02-15 16:40:59 -0500

    Made an ugly main menu

--- assets/stylesheet.lua
+++ assets/stylesheet.lua
@@ -3,3 +3,34 @@
 -- This software is released under the MIT License.
 -- https://opensource.org/licenses/MIT
 
+moonpie.graphics.font:register("assets/fonts/Black Window.ttf", "headline")
+moonpie.graphics.font:register("assets/fonts/Black Window.ttf", "headline-bold")
+
+moonpie.graphics.colors.light_shade = moonpie.graphics.colors.moss
+moonpie.graphics.colors.light_accent = moonpie.graphics.colors.meadow
+moonpie.graphics.colors.main = moonpie.graphics.colors.black
+moonpie.graphics.colors.dark_accent = moonpie.graphics.colors.thunder_cloud
+moonpie.graphics.colors.dark_shade = moonpie.graphics.colors.waterfall
+
+moonpie.graphics.colors.button_default = moonpie.graphics.colors.gray_medium
+moonpie.graphics.colors.button_default_hover = moonpie.graphics.colors.light_gray
+moonpie.graphics.colors.button_text = moonpie.graphics.colors.white
+moonpie.graphics.colors.primary = moonpie.graphics.colors.main
+moonpie.graphics.colors.info = moonpie.graphics.colors.steel_gray
+moonpie.graphics.colors.success = moonpie.graphics.colors.highland
+moonpie.graphics.colors.warning = moonpie.graphics.colors.zest
+moonpie.graphics.colors.danger = moonpie.graphics.colors.pomegranate
+
+local styles = moonpie.ui.styles
+
+styles.main_menu = {
+  background_color = "dark_accent",
+  width = 300,
+  height = 250,
+  opacity = 0.5,
+  padding = 10,
+}
+
+styles.title = {
+  font_size = 50
+}
--- game/ui/title_screen.lua
+++ game/ui/title_screen.lua
@@ -4,7 +4,18 @@
 -- https://opensource.org/licenses/MIT
 
 local title_screen = moonpie.ui.components.body({
-  moonpie.ui.components.h1({ text = "Welcome to Fairhaven", style = "align-center" })
+  moonpie.ui.components.section({
+    padding = 30,
+    moonpie.ui.components.h1({ text = "Welcome to Fairhaven", style = "title align-center" }),
+  }),
+  moonpie.ui.components.section({
+    id = "menu",
+    style = "main_menu align-center align-middle",
+    moonpie.ui.components.button({ caption = "New Game", width = "100%" }),
+    moonpie.ui.components.button({ caption = "Continue", width = "100%" }),
+    moonpie.ui.components.button({ caption = "Settings", width = "100%" }),
+    moonpie.ui.components.button({ caption = "Quit", width = "100%", click = function() love.event.quit() end })
+  })
 })
 
 return title_screen
