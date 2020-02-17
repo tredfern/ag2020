90e04aa58ddabf14c35ab581270232cc209024a5
Trevor Redfern <trevorredfern@gmail.com>
2020-02-17 08:25:14 -0500

    Updates to new game screen

--- assets/stylesheet.lua
+++ assets/stylesheet.lua
@@ -5,6 +5,7 @@
 
 moonpie.graphics.font:register("assets/fonts/Black Window.ttf", "headline")
 moonpie.graphics.font:register("assets/fonts/Black Window.ttf", "headline-bold")
+moonpie.graphics.font:register("assets/fonts/Abbasy Calligraphy Typeface.ttf", "handwriting")
 
 moonpie.graphics.colors.light_shade = moonpie.graphics.colors.convert_hex("#f3f3f3")
 moonpie.graphics.colors.light_accent = moonpie.graphics.colors.convert_hex("#ebaf25")
@@ -32,10 +33,37 @@ styles.main_menu = {
 }
 
 styles.menu_button = {
-  width = "100%"
+  width = "75%",
+  align = "center"
 }
 
 styles.menu_screen_title = {
   padding = 30,
   font_size = 50
+}
+
+styles.story_message = {
+  width = "80%",
+  background_color = "main",
+  padding = 10,
+  opacity = 0.7
+}
+
+styles.message_title = {
+  color = "dark_shade",
+  font_size = 40,
+  font_name = "Black Window",
+  margin = 5
+}
+
+styles.message_sub_title = {
+  color = "dark_shade",
+  font_size = 30,
+  font_name = "Black Window",
+  margin = 5
+}
+
+styles.message_text = {
+  font_size = 26,
+  font_name = "handwriting"
 }
